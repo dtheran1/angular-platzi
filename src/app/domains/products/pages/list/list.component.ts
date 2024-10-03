7e2f810eff61eeb6a566375051b5d08f568a0318
dtheran <dtheran1.0@gmail.com>
2024-10-03 09:11:19 -0500

    wip

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { ProductComponent } from './../../components/product/product.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })

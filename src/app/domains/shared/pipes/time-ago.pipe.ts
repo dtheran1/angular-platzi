ecbf1549deba94860ba3c732b59818bf8131d4fe
dtheran <dtheran1.0@gmail.com>
2024-10-08 09:26:23 -0500

    implement date-fns

---
+++ src/app/domains/shared/pipes/time-ago.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { formatDistance } from 'date-fns';
+
+@Pipe({
+  name: 'timeAgo',
+  standalone: true
+})
+export class TimeAgoPipe implements PipeTransform {
+
+  transform(value: string) {
+    const date = new Date(value);
+    const today = new Date();
+
+    const diff = formatDistance(today, date);
+    return diff;
+  }
+
+
+}

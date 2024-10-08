582513b401d86a302d7649b45cc18dec492b601d
dtheran <dtheran1.0@gmail.com>
2024-10-08 10:23:13 -0500

    routerLink

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { Product } from '../../model/product.model';
 import { CartService } from '../../model/services/cart.service';
-import { RouterLinkWithHref } from '@angular/router';
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLinkWithHref],
+  imports: [CommonModule, RouterLinkWithHref, RouterLinkActive],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'

b7dd747e0f9ac0f117f05f780286b814c60d3df2
dtheran <dtheran1.0@gmail.com>
2024-10-12 18:06:42 -0500

    wip

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, OnInit, signal } from '@angular/core';
 import { Product } from '@shared/model/product.model';
+import { CartService } from '@shared/model/services/cart.service';
 import { ProductService } from '@shared/model/services/product.service';
 
 @Component({
@@ -15,6 +16,7 @@ export class ProductDetailComponent implements OnInit {
   private productService = inject(ProductService);
   cover =signal<string>('');
   product = signal<Product | null>(null);
+  private cartService = inject(CartService);
 
   ngOnInit() {
     if(this.id) {
@@ -29,6 +31,12 @@ export class ProductDetailComponent implements OnInit {
 
   changeCover(image: string) {
     this.cover.set(image);
+  }
+
+addToCart() {
+    const product = this.product();
+    if (!product)  return;
+    this.cartService.addToCart(product);
   }
 
 }
